1b3846ce737191f8651bc031498bb7809119cb3d
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-03-03 12:15:26 +0100

    Add files via upload

---
+++ SinozicSebastianProvjeraZnanjaZadatak1/SinozicSebastianProvjeraZnanjaZadatak1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SinozicSebastianProvjeraZnanjaZadatak1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 3;
+            double d1, d2;
+
+            d1 = x / y;
+            d2 = x % y;
+
+
+            Console.WriteLine("d1= " + d1);
+            Console.WriteLine("d2= " + d2);
+           
+        }
+    }
+}
